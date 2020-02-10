ef89db4c4c24def080a074161a517138970951b3
Al Rempel <aa989190f363e46d@googlemail.com>
2020-02-10 17:40:30 +0300

    step 3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,13 @@
+from brain_games.scripts.cli import welcome_user
+
 def greeting():
-    print("Welcome to the Brain Games!")    
+    print("Welcome to the Brain Games!")
+    print()    
 
 def main():
     greeting()
+    welcome_user()
     
 if __name__ == "__main__":
     main()
+    
